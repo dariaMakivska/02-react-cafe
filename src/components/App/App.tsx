a0d4148dc6a8939291161cd60136d78a3eae91a9
Daria Makivska <peony.bluet@gmail.com>
2025-05-17 22:21:28 +0300

    Додає CafeInfo та votes.ts в App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,17 @@
-// import { useState } from 'react'
-
-import './App.module.css'
+import css from './App.module.css'
+import CafeInfo from '../CafeInfo/CafeInfo'
+import Votes, {Vote} from '../../types/votes'
+import { useState } from 'react'
 
 
 export default function App() {
-  return <div>Hello</div>;
+  const [votesState, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
+
+  return <div className={css.app}>
+    <CafeInfo/>
+  </div>
 }
