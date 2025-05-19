d09646392463f1fb2367c8f0182bee00b58e6cf4
Daria Makivska <peony.bluet@gmail.com>
2025-05-19 15:27:56 +0300

    Виправляє VoteOptions.tsx та votes.ts

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -8,10 +8,13 @@ interface VoteOptionsProps {
 }
 
 export default function VoteOptions({onVote, onReset, canReset,}: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote("good")}>Good</button>
+      <button className={css.button} onClick={() => onVote("neutral")}>Neutral</button>
+      <button className={css.button} onClick={() => onVote("bad")}>Bad</button>
+      {canReset && (
+        <button className={`${css.button} ${css.reset}`} onClick={onReset}>Reset</button>
+      )}</div>)
+}
 
-  return <div className={css.container}>
-  <button className={css.button} onClick={()=> onVote("good")}>Good</button>
-  <button className={css.button} onClick={()=> onVote("neutral")}>Neutral</button>
-  <button className={css.button} onClick={()=> onVote("bad")}>Bad</button>
-    <button className={`${css.button} ${css.reset}`} onClick={onReset} disabled={!canReset}>Reset</button>
-</div>}
